5b514a65a0fe0c58113239611ade617ee1c41e0f
TE <TE@localhost.localdomain>
2018-12-20 09:56:07 +0530

    Revised

--- Scheduling Algorithm/RoundRobin/src/mainFile.java
+++ Scheduling Algorithm/RoundRobin/src/mainFile.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class mainFile {
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws Exception{
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of processes: ");
