3871118897ac74f9c34b15a587bbb084ba4f2037
Aditya Mahaddalkar <adityam1311@gmail.com>
2019-02-10 11:23:35 +0530

    Assignment Complete

---
+++ D1/src/LRU.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class LRU {
+	public static void leastRecentlyUsed(ArrayList<Integer> pages, int capacity) {
+		
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		HashMap<Integer, Integer> leastRecentIndex = new HashMap<Integer, Integer>();
+		int page_faults = 0, iterator = 0;
+		for(int i : pages) {
+			if( queue.size() < capacity ) {
+				if(! queue.contains(i)) {
+					queue.add(i);
+					page_faults++;
+				}
+				leastRecentIndex.put(i, iterator);
+			}
+			else {
+				if(! queue.contains(i)) {
+					// Remove the least recently used page from the set
+					int lru = Integer.MAX_VALUE, val = Integer.MIN_VALUE;
+					for(int page : queue) {
+						if( leastRecentIndex.get(page) < lru) {
+							lru = leastRecentIndex.get(page);
+							val = page;
+						}
+					}
+					queue.remove(val);
+					queue.add(i);
+					page_faults++;
+				}
+				leastRecentIndex.put(i, iterator);
+			}
+			iterator ++;
+			
+			// For debugging print contents of queue
+			System.out.println("Elements of queue for iteration " + iterator);
+			for(int element : queue) {
+				System.out.println(element + " ");
+			}
+		}
+		System.out.println("Page faults: " + page_faults);
+	}
+	
+	public static void main(String args[]) {
+		int[] elements = {1, 2, 3, 3, 2, 0, 7, 7, 4, 3, 4, 6, 3, 5};
+		ArrayList<Integer> pages = new ArrayList<Integer>();
+		for(int i = 0; i < elements.length;i ++) {
+			pages.add(elements[i]);
+		}
+		int capacity = 4;
+		leastRecentlyUsed(pages, capacity);
+	}
+}
---
+++ D1/src/OptimalReplacement.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class OptimalReplacement {
+	
+	public static void optimalReplacement(ArrayList<Integer> pages, int capacity) {
+		
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		int iteration = 0, page_fault = 0;
+		
+		for(int i = 0;i < pages.size();i ++) {
+			
+			if(queue.size() < capacity) {
+				
+				if(!queue.contains(pages.get(i))) {
+					queue.add(pages.get(i));
+					page_fault ++;
+				}
+			}
+			else {
+				if(!queue.contains(pages.get(i))) {
+					
+					//Find the page that will be accessed at the largest duration of time in the future
+					int farthest_page_index = 0;
+					int time = get_future_time(pages, i, queue.get(farthest_page_index)), temp_time;
+					for(int index = 0;index < queue.size(); index ++) {
+						temp_time = get_future_time(pages, i, queue.get(index));
+						if(temp_time == -1) {
+							time = temp_time;
+							farthest_page_index = index;
+						}
+						else if(time != -1 && temp_time > time) {
+							time = temp_time;
+							farthest_page_index = index;
+						}
+					}
+					queue.remove(farthest_page_index);
+					queue.add(pages.get(i));
+					page_fault ++;
+				}
+			}
+			
+			//For debugging
+			System.out.println("Contents of queue at iteration " + iteration);
+			for(int page: queue) {
+				System.out.println(page);
+			}
+			iteration ++;
+		}
+		
+		System.out.println("Page faults: " + page_fault);
+	}
+
+	private static int get_future_time(ArrayList<Integer> pages, int current_page_index, int farthest_page) {
+		
+		for(int i = current_page_index;i < pages.size();i ++) {
+			if(pages.get(i) == farthest_page) {
+				return i - current_page_index;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String args[]) {
+		ArrayList<Integer> pages = new ArrayList<Integer>();
+		int [] elements = {2, 3, 4, 2, 1, 3, 7, 5, 4, 3};
+		for(int i = 0;i < elements.length;i ++) {
+			pages.add(elements[i]);
+		}
+		int capacity = 3;
+		optimalReplacement(pages, capacity);
+	}
+}
